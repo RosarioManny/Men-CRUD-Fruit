d58e581d21c010d51cf7ea9d8897f037c53ad88f
RosarioManny <rosario.emm47@gmail.com>
2024-09-26 15:34:13 -0400

    Navigation and Fruits List

--- server.js
+++ server.js
@@ -23,14 +23,20 @@ app.get("/", async (req, res) => { // ROOT/MAIN ROUTE
 app.post("/fruits", async (req, res) => {
     if(req.body.ripe === "on") { // This is done because the check box used, isn't a true or false value. So if checked it will be set to a boolean of true.
         req.body.ripe = true; 
-    } else {
-        req.body.ripe = false;
-    }
+        } else {
+            req.body.ripe = false;
+         }
     
     await Fruit.create(req.body);
 
-    res.redirect("/fruits/new");
+    res.redirect("/fruits");
 });
+
+app.get("/fruits", async (req, res) => {
+    const allFruits = await Fruit.find();
+    res.render("fruits/index.ejs", { fruits: allFruits })
+});
+
 
 app.get("/fruits/new", (req, res) => { // NEW FRUITS ROUTE
     res.render("fruits/new.ejs")
