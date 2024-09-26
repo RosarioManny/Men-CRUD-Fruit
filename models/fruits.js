bee29d20c5bcbf5d5816033bedb9e1bbe46b5cf4
RosarioManny <rosario.emm47@gmail.com>
2024-09-26 11:19:55 -0400

    Notes added

--- models/fruits.js
+++ models/fruits.js
@@ -1,9 +1,9 @@
 const mongoose = require("mongoose")
 const fruitSchema = new mongoose.Schema({
-    name: String,
+    name: String, 
     ripe: Boolean,
-})
+}) // This is defining the Schema for Mongoose
 
-const Fruit = mongoose.model("Fruits", fruitSchema) // creates model
+const Fruit = mongoose.model("Fruits", fruitSchema) // Creates model
 
 module.exports = Fruit // This is exporting the Fruit variable above ^^^
--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 const dotenv = require("dotenv");
 dotenv.config();
-const express = require("express");
-const mongoose = require("mongoose"); 
+
+const express = require("express"); // Using the Express server
 const app = express();
 
-mongoose.connect(process.env.MONGODB_URI);
+const mongoose = require("mongoose"); 
+mongoose.connect(process.env.MONGODB_URI); // Looks at the .env/MONGODB_URI for the route to connect
+
+
 mongoose.connection.on("connected", () => {
-    console.log(`Conneted to MongoDB ${mongoose.connection.name}.`)
+    console.log(`Conneted to MongoDB ${mongoose.connection.name}.`) // Test the Mongoose connection
 });
 const Fruit = require("./models/fruits.js");
 
